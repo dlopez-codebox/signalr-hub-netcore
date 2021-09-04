b296b8676cdf4d7d9143346901d5adbf09e37947
dlopez@codebox-peru.com <dlopez@codebox-peru.com>
2021-09-04 20:59:17 +0000

    Initial

---
+++ signalr/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using signalr.Hubs.Clients;
+using signalr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signalr.Hubs
+{
+    public class ChatHub : Hub<IChatClient>
+    {
+
+        public async Task SendMessage(ChatMessage message) {
+
+            await Clients.All.ReceiveMessage(message);
+        
+        }
+    }
+}
---
+++ signalr/Hubs/Clients/IChatClient.cs
@@ -0,0 +1,14 @@
+﻿using signalr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signalr.Hubs.Clients
+{
+    public interface IChatClient
+    {
+        Task ReceiveMessage(ChatMessage message);
+
+    }
+}
